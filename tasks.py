1b9668a7729df4b825b4601b03b4cbb48926025e
namgivu <namgivu@gmail.com>
2017-10-07 16:16:45 +0700

    positional param

--- tasks.py
+++ tasks.py
@@ -16,3 +16,20 @@ def build(ctx, clean=False):
     print("Cleaning!")
   print("Building!")
 
+
+
+@task
+def hi(ctx, name):
+  """
+  name='Nam'
+  invoke build -n $name
+  invoke build -n$name
+
+  invoke build --name $name
+  invoke build --name=$name
+
+  #no-default-value param can become positional param
+  invoke build $name
+  """
+  displayName = '%s' % name
+  print("Hi %s!" % displayName )
