b3a56997b7013a4affd56794fc702863945902bf
namgivu <namgivu@gmail.com>
2017-10-08 17:57:16 +0700

    nested collection

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -14,7 +14,9 @@ c4 = Collection(task00, task01a, task01b, task02)
 
 #add child to root
 ns = namespace = Collection() #the root node
+
 ns.add_collection(c1, name='c1')
+c1.add_collection(c3, name='c3')
+
 ns.add_collection(c2, name='c2')
-ns.add_collection(c3, name='c3')
 ns.add_collection(c4, name='c4')
