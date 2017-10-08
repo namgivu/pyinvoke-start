2e48bd6e394b49556424a6afcee0c00ea3d2b753
namgivu <namgivu@gmail.com>
2017-10-08 17:43:33 +0700

    put task into collection/namespace

--- README.md
+++ README.md
@@ -6,3 +6,13 @@ source ./venv3/bin/activate
 
 The practice/get-started guide is referenced here 
 http://docs.pyinvoke.org/en/latest/getting_started.html
+
+Put task into collection/namespace
+http://docs.pyinvoke.org/en/latest/concepts/namespaces.html#nesting-collections
+set default task for namespace
+http://docs.pyinvoke.org/en/latest/concepts/namespaces.html#default-tasks
+
+Print all tasks
+```
+invokde --list
+```
--- tasks.py
+++ tasks.py
@@ -105,3 +105,18 @@ pass
 
 pass
 ##endregion define task
+
+from invoke import Collection
+ns = namespace = Collection() #the root node
+
+#the child node
+c1 = Collection(buildA, buildB)
+c2 = Collection(hiA, hiB, hiC)
+c3 = Collection(sysTime)
+c4 = Collection(task00, task01a, task01b, task02)
+
+#add child to root
+ns.add_collection(c1, name='c1')
+ns.add_collection(c2, name='c2')
+ns.add_collection(c3, name='c3')
+ns.add_collection(c4, name='c4')
