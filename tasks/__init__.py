847c80ed4b462a59a13b26d369a331f4b9cd7d78
namgivu <namgivu@gmail.com>
2017-10-08 18:06:34 +0700

    TODO

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -10,3 +10,5 @@ ns = namespace = Collection() #the root node
 ns.add_collection(c1, name='c1')
 ns.add_collection(c2, name='c2')
 ns.add_collection(c4, name='c4')
+
+#TODO how to add c1.c3 to root at module level? We can do it by defining collection n1 va n3 manually at this commit https://github.com/namgivu/pyinvoke-start/commit/b3a56997b7013a4affd56794fc702863945902bf
