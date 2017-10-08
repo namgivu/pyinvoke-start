a3c4a21e94a6b4464ece25d59a9d8c6f9078135d
namgivu <namgivu@gmail.com>
2017-10-08 18:02:59 +0700

    tasks as a package - import from module
    tasks as a package - import from module

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -1,22 +1,12 @@
 from invoke import Collection
 
 #the child node
-from tasks.c1 import *
-from tasks.c2 import *
-from tasks.c3 import *
-from tasks.c4 import *
-
-c1 = Collection(buildA, buildB)
-c2 = Collection(hiA, hiB, hiC)
-c3 = Collection(sysTime)
-c4 = Collection(task00, task01a, task01b, task02)
-
+import tasks.c1
+import tasks.c2
+import tasks.c4
 
 #add child to root
 ns = namespace = Collection() #the root node
-
 ns.add_collection(c1, name='c1')
-c1.add_collection(c3, name='c3')
-
 ns.add_collection(c2, name='c2')
 ns.add_collection(c4, name='c4')
